4fae500d2e90526fa9a1d6d1e78a0245772c2c53
huzhongyuan <1141422520@qq.com>
2018-08-16 12:15:18 +0800

    主页数据加载

--- html/common/foot.js
+++ html/common/foot.js
@@ -1,25 +1,28 @@
 let foot ='<footer class="aui-bar aui-bar-tab footer" id="footer">' +
-'<div class="aui-bar-tab-item footerNav tohome" tapmode>' +
-    '<img src="./../images/home1.png">' +
-    '<div class="aui-bar-tab-label">首页</div>' +
-'</div>' +
-'<div class="aui-bar-tab-item footerNav toteam" tapmode>' +
-    '<img src="./../images/team.png">' +
-    '<div class="aui-bar-tab-label">团队</div>'+
-'</div>' +
-'<div class="aui-bar-tab-item footerNav tocase" tapmode>' +
-    '<img src="./../images/case.png">' +
-    '<div class="aui-bar-tab-label">案例</div>' +
-'</div>' +
-'<div class="aui-bar-tab-item footerNav tocompany" tapmode>' +
-    '<img src="./../images/company.png">' +
-    '<div class="aui-bar-tab-label">我们</div>' +
-'</div>' +
-'</footer>';
+
+'</footer>' +
+'</body>' +
+'</html>';
 document.write(foot);
 
+
+// 设置常数
+const baseUrl = 'http://118.24.159.161:8083/h5';
+
+// 获取基本信息
+let timeStamp = Date.parse(new Date());
+let token = timeStamp + 'hopynrztoken';
+token = md5(token).toUpperCase();
+
+// 设置接口token
+$(document).ajaxSend(function (event, xhr) {
+    xhr.setRequestHeader("token", token);
+    xhr.setRequestHeader("timeStamp", timeStamp);
+});
+
+
 //点击底部导航跳转
-let footerNav = () => {
+let footNav = () => {
     let tohome = document.getElementsByClassName('tohome')[0];
     let toteam = document.getElementsByClassName('toteam')[0];
     let tocase = document.getElementsByClassName('tocase')[0];
@@ -37,4 +40,63 @@ let footerNav = () => {
         window.location.href = './company.html';
     }
 }
-footerNav();
+// sessionStorage.setItem('resFooter', '');
+// console.log(sessionStorage.getItem('resFooter'));
+
+
+//写入头部和底部数据
+let writeCommon = (resFooter, resHeader) => {
+    let footerNav = document.createElement('div');
+    footerNav.innerHTML = '<div class="aui-bar-tab-item footerNav tohome" tapmode>' +
+        '<img src="' + resFooter[0].addImgUrl + '" class="homeImg" style="width:1rem;height:1rem;">' +
+        '<div  class="aui-bar-tab-label">' + resFooter[0].columnName + '</div>' +
+    '</div>' +
+    '<div class="aui-bar-tab-item footerNav toteam" tapmode>' +
+        '<img src="' + resFooter[1].addImgUrl + '" style="width:1rem;height:1rem;" class="teamImg">' +
+        '<div class="aui-bar-tab-label">' +resFooter[1].columnName+ '</div>'+
+    '</div>' +
+    '<div class="aui-bar-tab-item footerNav tocase" tapmode>' +
+        '<img src="' + resFooter[2].addImgUrl + '"  style="width:1rem;height:1rem;" class="caseImg">' +
+        '<div class="aui-bar-tab-label">' + resFooter[2].columnName + '</div>' +
+    '</div>' +
+    '<div class="aui-bar-tab-item footerNav tocompany" tapmode>' +
+        '<img src="' + resFooter[3].addImgUrl + '"  style="width:1rem;height:1rem;" class="companyImg">' +
+        '<div class="aui-bar-tab-label">' + resFooter[3].columnName + '</div>' +
+    '</div>';
+    let footer = document.getElementById('footer');
+    footer.style.backgroundColor = 'rgba(0,0,0,1)';
+    footer.appendChild(footerNav);
+    let header = document.getElementsByClassName('header')[0];
+    header.innerHTML = resHeader.name;
+}
+
+// sessionStorage.setItem('resFooter', '');
+//储存数据
+if(sessionStorage.getItem('resFooter') && sessionStorage.getItem('resFooter')) {
+    //console.log(1111);
+    let resFooter = sessionStorage.getItem('resFooter');
+    let resHeader = sessionStorage.getItem('resHeader');
+    resFooter = JSON.parse(resFooter);
+    resHeader = JSON.parse(resHeader);
+    writeCommon(resFooter, resHeader);
+    footNav();
+}else {
+//首次进入界面加载数据
+let footerImg = new Promise((resolve, reject) => {
+    $.ajax({
+        url: baseUrl + '/siteInfo',
+        dataType: 'json',
+        method: 'get',
+        success: function (res) {
+        writeCommon(res.h5Footer, res.siteInfo);
+        sessionStorage.setItem('resFooter', JSON.stringify(res.h5Footer));
+        sessionStorage.setItem('resHeader', JSON.stringify(res.siteInfo));
+        footNav();
+        }
+    })
+})
+}
+
+
+
+// footerNav();
--- html/common/head.js
+++ html/common/head.js
@@ -1,16 +1,29 @@
+
 (function() {
   let cssArr = document.getElementsByTagName('script');
   headTitle = cssArr[0].getAttribute('data-title');
-  let head = '<header class="aui-bar aui-bar-nav header">雅鸟设计</header>';
+  let head = '<header class="aui-bar aui-bar-nav header"></header>';
 
-  let headStyle =  '<link rel="stylesheet" type="text/css" href="../css/module/aui.css" />' +
+  let headStyle =  
+    '<meta name="robots" content="all">' + 
+    '<meta name="author" content="w3school.com.cn">' + 
+    '<link rel="icon" type="image/png" sizes="16x16" href="./../images/home.png">' +
+    '<meta name="viewport" content="maximum-scale=1.0,minimum-scale=1.0,user-scalable=0,width=device-width,initial-scale=1.0"/>' +
+    '<meta name="format-detection" content="telephone=no,email=no,date=no,address=no">' +
+    '<meta name="apple-mobile-web-app-capable" content="yes">' +
+    '<title>雅鸟设计</title>' +
+  '</head>' + 
+  '<body>' +
+  '<link rel="stylesheet" type="text/css" href="../css/module/aui.css" />' +
   '<link rel="stylesheet" href="./../css/module/header.css">' +
   '<link rel="stylesheet" href="./../css/module/footer.css">' +
   '<link rel="stylesheet" href="./../css/pages/' + headTitle + '.css">' +
   '<script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>' +
-  '<script src="./../js/module/utils.js"></script>' +
-  '<script src="./../js/pages/' + headTitle + '.js"></script>';
+  '<script src="./../js/lib/jquery-3.1.1.min.js"></script>' +
   '<script src="./../js/module/md5.js"></script>' +
+  '<script src="./../js/pages/' + headTitle + '.js"></script>';
   document.write(headStyle);
   document.write(head);
+
+
 })()
